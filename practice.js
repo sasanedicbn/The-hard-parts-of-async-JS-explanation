42ad5fa8263c5f870c3b3bd4bf1fb5dcb2272f87
Sasa Nedic <svetasmak0@gmail.com>
2025-03-01 17:51:07 +0100

    Create hight order fn with callback

--- practice.js
+++ practice.js
@@ -1,1 +1,15 @@
-console.log("sasa");
+// HIGHT ORDER FUNCTION EXAMPLES
+
+// 3. HIHGT ORDER FUNCTION => CALLBACK
+const fetchData = (callback) => {
+  setTimeout(() => {
+    const data = { name: "Sasa", coutry: "SRB" };
+    callback(data);
+  }, 2000);
+};
+
+const callbackFn = (user) => {
+  return console.log(user);
+};
+
+fetchData(callbackFn);
