1c1c08b912401e27d42028d81d7e1db3a54c210e
Sasa Nedic <svetasmak0@gmail.com>
2025-03-01 20:03:21 +0100

    Hosting with variables

--- practice.js
+++ practice.js
@@ -1,15 +1,50 @@
-// HIGHT ORDER FUNCTION EXAMPLES
+// // HIGHT ORDER FUNCTION EXAMPLES
+// // 1. FUNCTION AS ARGUMENT
+// const numbers = [1, 2, 3, 4, 5];
 
-// 3. HIHGT ORDER FUNCTION => CALLBACK
-const fetchData = (callback) => {
-  setTimeout(() => {
-    const data = { name: "Sasa", coutry: "SRB" };
-    callback(data);
-  }, 2000);
-};
+// const doubleFN = (x) => x * 2;
+// const doubledNumbers = numbers.map(doubleFN);
 
-const callbackFn = (user) => {
-  return console.log(user);
-};
+// // 2. FUNCTION RETURN FUNCTION
 
-fetchData(callbackFn);
+// // 3. HIHGT ORDER FUNCTION => CALLBACK
+// const fetchData = (callback) => {
+//   setTimeout(() => {
+//     const data = { name: "Sasa", coutry: "SRB" };
+//     callback(data);
+//   }, 2000);
+// };
+
+// const callbackFn = (user) => {
+//   return console.log(user);
+// };
+
+// fetchData(callbackFn);
+
+// 4.
+// function createMultiplier(multiplier) {
+//   return function unutrasnja(number) {
+//     return number * multiplier;
+//   };
+// }
+
+// const double = createMultiplier(2);
+// const triple = createMultiplier(3);
+
+// console.log(double);
+// console.log(triple(5));
+
+// =================================================================
+// HOISTING
+
+//hoisting with var
+
+// console.log(x);  => undefined
+// var x = 5; => inicijalizovano
+// console.log(x); => 5
+
+// hoisting with let and const
+
+// console.log(y); // ReferenceError: Cannot access 'y' before initialization
+// let y = 10;
+// console.log(y); // 10
