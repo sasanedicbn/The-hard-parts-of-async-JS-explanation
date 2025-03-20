5fc8616d18adebdbff092587abbe35cf233e8eda
Sasa Nedic <svetasmak0@gmail.com>
2025-03-20 18:16:43 +0100

    Create sortArray witout sort key word

--- practice.js
+++ practice.js
@@ -810,8 +810,16 @@
 //  do najvećeg bez korištenja ugrađene sort funkcije.
 // Primjer: Za niz [3, 1, 4, 2], rezultat je [1, 2, 3, 4].
 function sortArray(arr) {
-  const result = arr.sort((a, b) => a - b);
-  return result;
+  const n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
 }
+
 const result = sortArray([1, 6, 3, 4, 5, 2]);
-console.log(result, "result");
+console.log(result, "result"); // [1, 2, 3, 4, 5, 6]
