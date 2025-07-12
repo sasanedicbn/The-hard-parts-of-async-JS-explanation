bf5fee68d9c6901bb0ebd75753f18ab76d4d3f69
Sasa Nedic <svetasmak0@gmail.com>
2025-07-12 20:28:48 +0200

    Worrking on task

--- practice.js
+++ practice.js
@@ -990,3 +990,7 @@
 // Napiši funkciju koja prima dva stringa i provjerava jesu li oni anagrami
 // (sadrže iste znakove, ali u različitom redoslijedu).
 // Primjer: "listen" i "silent" su anagrami.
+function provjeraAnagrama(word1, word2) {
+  const first = word1.split("").sort().join("");
+  const second = word2.split("").sort().join("");
+}
